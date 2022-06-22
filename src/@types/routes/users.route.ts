196f3065762390da27f62c919ccb3a3c513d48ae
Bernardo Lima <bernardomonclaro@gmail.com>
2022-06-22 13:22:03 -0300

    primeiro commit

---
+++ src/@types/routes/users.route.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { StatusCodes } from 'http-status-codes';
+// get /users
+// get /users/:uuid
+// post /users
+// put /users/:uuid
+// delete /users/:uuid
+
+const usersRoute = Router();
+
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+  const users = [{ userName: 'Bernardo'}];
+  res.status(StatusCodes.OK).send(users);
+});
+
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  res.status(StatusCodes.OK).send({ uuid });
+});
+
+export default usersRoute;
